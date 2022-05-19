1f2fa394fc2512971d87ed4224bbecbd160c0351
Mafe Garces <mafegarces@hotmail.com>
2022-05-18 21:01:39 -0500

    Add rotation and clear rows

--- app/src/components/Tetris.js
+++ app/src/components/Tetris.js
@@ -17,7 +17,7 @@ const Tetris = () => {
     const [dropTime, setDropTime] = useState(null);
     const [gameOver, setGameOver] = useState(false);
 
-    const [player, updatePlayerPosition, resetPlayer] = usePlayer();
+    const [player, updatePlayerPosition, resetPlayer, playerRotate] = usePlayer();
     const [stage, setStage] = useStage(player, resetPlayer);
 
     const movePlayer = (direction) => {
@@ -57,6 +57,8 @@ const Tetris = () => {
                 movePlayer(1);
             } else if (keyCode === 40) {
                 dropPlayer();
+            } else if (keyCode === 38) {
+                playerRotate(stage, 1);
             }
         }
     }
--- app/src/hooks/usePlayer.js
+++ app/src/hooks/usePlayer.js
@@ -1,6 +1,7 @@
 import { useCallback, useState } from "react";
 
 import { TETROMINOES, getRandomTetrominoe } from "../common/constants/tetrominoes";
+import { checkCollision } from "../common/helpers/checkCollision";
 import { STAGE_WIDTH } from "../common/helpers/createStage";
 
 export const usePlayer = () => {
@@ -9,6 +10,37 @@ export const usePlayer = () => {
         tetrominoe: TETROMINOES[0].shape,
         collided: false,
     });
+
+    const rotate = (matrix, direction) => {
+
+        // Transpose matrix
+        const rotatedTetro = matrix.map((_, index) =>
+            matrix.map(col => col[index]),
+        );
+
+        // Reverse each row
+        if (direction > 0) return rotatedTetro.map(row => row.reverse());
+        return rotatedTetro.reverse();
+    }
+
+    const playerRotate = (stage, direction) => {
+        const clonedPlayer = { ...player };
+        clonedPlayer.tetrominoe = rotate(clonedPlayer.tetrominoe, direction);
+
+        const pos = clonedPlayer.position.x;
+        let offset = 1;
+        while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+            clonedPlayer.position.x += offset;
+            offset = -(offset + (offset > 0 ? 1 : -1));
+            if (offset > clonedPlayer.tetrominoe[0].length) {
+                rotate(clonedPlayer.tetrominoe, -direction);
+                clonedPlayer.position.x = pos;
+                return;
+            }
+        }
+
+        setPlayer(clonedPlayer);
+    }
 
     const updatePlayerPosition = ({ x, y, collided }) => {
         setPlayer((prev) => ({
@@ -26,5 +58,5 @@ export const usePlayer = () => {
         });
     }, []);
 
-    return [player, updatePlayerPosition, resetPlayer];
+    return [player, updatePlayerPosition, resetPlayer, playerRotate];
 };
